a27cc2ac952ceb8cf21ebedad17b5a90400ea694
Julia <juliavolk96@gmail.com>
2023-03-30 20:37:10 +0400

    Задача №2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,1 +1,17 @@
-var clicker__counter = document.getElementById("clicker__counter")
+var num = 0;
+var clicker__counter = document.getElementById("clicker__counter");
+var cookie = document.getElementById("cookie");
+
+function cookieClick() {
+  num++;
+
+  clicker__counter.innerHTML = num;
+  changeSize();
+}
+
+function changeSize() {
+  cookie.style.width = 230 + 'px';
+  setTimeout(function() {
+    cookie.style.width = 200 + 'px';
+  }, 100)
+}

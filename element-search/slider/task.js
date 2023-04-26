5a6df78d35763d8148b889ce973fc846e70f4947
Julia <juliavolk96@gmail.com>
2023-04-26 21:55:55 +0400

    Задача №3

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,24 +1,20 @@
 const sliderItems = document.querySelectorAll(".slider__item");
 const sliderArrows = document.querySelectorAll(".slider__arrow");
-console.log(sliderItems)
-console.log(sliderArrows)
-let indexImage = 0;
-let numOfImages = sliderItems.length;
-
-function sliderAction(arrowClass) {
-  sliderItems.item(indexImage).classList.remove("slider__item_active");
-  if (arrowClass == "slider__arrow_prev") {
-    indexImage = (indexImage - 1 + numOfImages) % numOfImages;
-  } else {
-    indexImage = (indexImage + 1) % numOfImages;
-  }
-  sliderItems.item(indexImage).classList.add("slider__item_active");
-}
-
-for(let i = 0; i < sliderArrows.length; i++) {
+// console.log(sliderItems)
+// console.log(sliderArrows)
+for (let i = 0; i < sliderArrows.length; i++) {
   sliderArrows[i].addEventListener('click', function(e) {
-    // sliderAction(sliderArrows[i].classList[1]);
-    sliderAction(e.target.classList[1])
+    const arrowClass = e.target.classList[1];
+    const activeIndex = Array.from(sliderItems).findIndex(item => item.classList.contains("slider__item_active"));
+    sliderItems[activeIndex].classList.remove("slider__item_active");
+    let indexImage = 0;
+    const numOfImages = sliderItems.length;
+    if (arrowClass == "slider__arrow_prev") {
+      indexImage = (activeIndex - 1 + numOfImages) % numOfImages;
+    } else {
+      indexImage = (activeIndex + 1) % numOfImages;
+    }
+    sliderItems[indexImage].classList.add("slider__item_active");
   });
 }
 

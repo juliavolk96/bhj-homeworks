d6b2e34613bb32a55103141e058de3a7d36a4464
Julia <juliavolk96@gmail.com>
2023-04-26 15:20:53 +0400

    Задача №2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,19 +1,23 @@
-'use strict'
+'use strict';
 
 const menuSub = document.querySelectorAll('.menu_sub');
 const menuLink = document.querySelectorAll('.menu__link');
 
-for(let i = 0; i < menuLink.length; i++) {
-  menuLink[i].addEventListener('click', function(e){
-    e.preventDefault();
+for (let i = 0; i < menuLink.length; i++) {
+  menuLink[i].addEventListener('click', function(e) {
     const subMenu = this.parentElement.querySelector('.menu_sub');
-    console.log(subMenu)
+    const activeSubMenus = document.querySelectorAll('.menu_active');
+
+    activeSubMenus.forEach(function(activeSubMenu) {
+      if (activeSubMenu !== subMenu) {
+        activeSubMenu.classList.remove('menu_active');
+      }
+    });
+
     if (subMenu) {
-      subMenu.classList.add('menu_active');
+      e.preventDefault();
+      subMenu.classList.toggle('menu_active');
     } 
   });
 }
-
-
-
 
